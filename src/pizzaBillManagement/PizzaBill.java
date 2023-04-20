12cd1d3865eaec6858a739277f02bcf36b659a4c
sojan Jose <sojanjose@ymail.com>
2023-04-20 18:58:29 -0400

    pizza billing is being updated as requested

--- src/pizzaBillManagement/PizzaBill.java
+++ src/pizzaBillManagement/PizzaBill.java
@@ -1,65 +1,86 @@
 package pizzaBillManagement;
+
 import java.util.Scanner;
+
 public class PizzaBill {
-	String pizzaSize[]= {"Small cheese pizza ","Medium cheese pizza","Large cheese pizza"};
+
 	double pizzaBasePrice;
 	double pepperoniTopingPrice;
 	double xtraCheesePrice;
-	String selectedPizzaSize;
-	char YorNpepperoni;
-	char YorNcheese;
-	double totalPrice;
-	void takePizzaOrder() {
+//Take pizza order
+	String takePizzaOrder(Scanner sc, String pizzaSize[]) {
 		System.out.println("Available Cheese pizza size:");
-		for(int i=0;i<pizzaSize.length;i++) {
+		for (int i = 0; i < pizzaSize.length; i++) {
 			System.out.println(pizzaSize[i]);
 		}
-		Scanner sc =new Scanner(System.in);
 		System.out.println("Please select your pizza size:");
-		selectedPizzaSize=sc.nextLine();
-		System.out.println("Do you want pepperoni  topping(Y/N?");
-	   YorNpepperoni=sc.next().charAt(0);
-	   System.out.println("Do you want extra cheeese(Y/N?");
-	   YorNcheese=sc.next().charAt(0);	
+		String selectedPizzaSize = sc.nextLine();
+		return selectedPizzaSize;
 	}
-	void priceCalculate() {
-		switch(selectedPizzaSize) {
-		case  "Small cheese pizza":
-			pizzaBasePrice=15;
-			if(YorNpepperoni=='Y') {
-				pepperoniTopingPrice=2;
+//check selected pizza is from the list
+	boolean checkSelection(String pizzaSize[], String selection) {
+		boolean listedSelection = false;
+		for (int i = 0; i < pizzaSize.length; i++) {
+			if (pizzaSize[i].equalsIgnoreCase(selection)) {
+				listedSelection = true;
+				break;
+			}
+		}
+		return listedSelection;
+	}
+//confirm if pepperoni toppings to be added
+	boolean confirmPepperonitop(Scanner sc) {
+		char confirm;
+		System.out.println("Do you want pepperoni  topping(Y/N?");
+
+		if ((confirm = sc.next().charAt(0)) == 'Y') {
+			return true;
+		} else
+			return false;
+	}
+//confirm if xtra cheese required
+	boolean confirmExtraCheese(Scanner sc) {
+		char confirm;
+		System.out.println("Do you want extra cheeese(Y/N?");
+		if ((confirm = sc.next().charAt(0)) == 'Y') {
+			return true;
+		} else
+			return false;
+	}
+//calculate total price
+	double priceCalculate(String selection, boolean pepperoni, boolean cheese) {
+		double totalPrice = 0;
+		switch (selection) {
+		case "Small cheese pizza":
+			pizzaBasePrice = 15;
+			if (pepperoni) {
+				pepperoniTopingPrice = 2;
 			}
 			break;
 		case "Medium cheese pizza":
-			pizzaBasePrice=20;
-			if(YorNpepperoni=='Y') {
-				pepperoniTopingPrice=3;
+			pizzaBasePrice = 20;
+			if (pepperoni) {
+				pepperoniTopingPrice = 3;
 			}
 			break;
 		case "Large cheese pizza":
-			pizzaBasePrice=25;
-			if(YorNpepperoni=='Y') {
-				pepperoniTopingPrice=2;
+			pizzaBasePrice = 25;
+			if (pepperoni) {
+				pepperoniTopingPrice = 2;
 			}
-			break;						
+			break;
 		}
-		if(YorNcheese=='Y') {
-			xtraCheesePrice=1;	
-		}
-		totalPrice=pizzaBasePrice+pepperoniTopingPrice+xtraCheesePrice;
-	}
-	void printTheBill() {
-		System.out.println(selectedPizzaSize+"------------------$"+pizzaBasePrice);
-		if(YorNpepperoni=='Y') {
-			System.out.println("Pepperoni toppings"+"---------------$"+pepperoniTopingPrice);	
-		}
-		if(YorNcheese=='Y') {
-			System.out.println("Xtra Cheese            "+"----------------- $"+xtraCheesePrice);
-		}
-			System.out.println("total price                "+"-----------------$"+totalPrice);
-		
-	}
-	
-	
 
+		if (cheese) {
+			xtraCheesePrice = 1;
+		}
+		totalPrice = pizzaBasePrice + pepperoniTopingPrice + xtraCheesePrice;
+		return totalPrice;
+	}
+// print the total price
+	void printTheBill(double totalPrice, String selection) {
+
+		System.out.println("total price for the " + selection + "-------$" + totalPrice);
+
+	}
 }
--- src/pizzaBillManagement/PizzaBillMain.java
+++ src/pizzaBillManagement/PizzaBillMain.java
@@ -1,14 +1,22 @@
 package pizzaBillManagement;
 
+import java.util.Scanner;
+
 public class PizzaBillMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		PizzaBill bill=new PizzaBill();
-		bill.takePizzaOrder();
-		bill.priceCalculate();
-		bill.printTheBill();
-
+		String selection;
+		String pizzaSize[] = { "Small cheese pizza", "Medium cheese pizza", "Large cheese pizza" };
+		Scanner sc = new Scanner(System.in);
+		PizzaBill bill = new PizzaBill();
+		selection = bill.takePizzaOrder(sc, pizzaSize);
+		if (bill.checkSelection(pizzaSize, selection)) {
+			double totalPrice = bill.priceCalculate(selection, bill.confirmPepperonitop(sc),
+					bill.confirmExtraCheese(sc));
+			bill.printTheBill(totalPrice, selection);
+		} else
+			System.out.println("your selection " + selection + " is not in the list");
 	}
 
 }
